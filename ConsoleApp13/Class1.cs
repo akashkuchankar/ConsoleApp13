09e3a1fd87823e15eb4b4d6f37f4dba57be1a0ce
akashkuchankar <kuchankarakash@gmail.com>
2022-10-20 01:38:39 -0700

    arrary related

--- ConsoleApp13/Class1.cs
+++ ConsoleApp13/Class1.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ConsoleApp13
-{
-   public class Book
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Publisher { get; set; }   
-        public double Price { get; set; }   
-    }
-    public class Course
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Duration { get; set; }
-        public double Fees { get; set; }
-    }
+//namespace ConsoleApp13
+//{
+//   public class Book
+//    {
+//        public int Id { get; set; }
+//        public string Name { get; set; }
+//        public string Publisher { get; set; }   
+//        public double Price { get; set; }   
+//    }
+//    public class Course
+//    {
+//        public int Id { get; set; }
+//        public string Name { get; set; }
+//        public double Duration { get; set; }
+//        public double Fees { get; set; }
+//    }
 
-}
+//}
